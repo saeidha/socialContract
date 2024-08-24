6b568160932040aeff4e6dd26efe60907a635856
saeed madi <saeed221373@yahoo.com>
2024-08-24 22:01:12 +0330

    add test

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,36 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract SocialMedia {
+    struct Post {
+        uint id;
+        address author;
+        string content;
+        uint timestamp;
+    }
+
+    Post[] public posts;
+    uint public postCount = 0;
+
+    event PostCreated(
+        uint id,
+        address indexed author,
+        string content,
+        uint timestamp
+    );
+
+    function createPost(string memory _content) public {
+        postCount++;
+        posts.push(Post(postCount, msg.sender, _content, block.timestamp));
+        emit PostCreated(postCount, msg.sender, _content, block.timestamp);
+    }
+
+    function getPost(uint _id) public view returns (Post memory) {
+        require(_id > 0 && _id <= postCount, "Post does not exist");
+        return posts[_id - 1];
+    }
+
+    function getAllPosts() public view returns (Post[] memory) {
+        return posts;
+    }
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -5,19 +5,88 @@ import "forge-std/Test.sol";
 
 import "src/Contract.sol";
 
-contract TestContract is Test {
-    Contract c;
+contract SocialMediaTest is Test {
+    SocialMedia socialMedia;
 
     function setUp() public {
-        c = new Contract();
+        socialMedia = new SocialMedia();
     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
+    function testCreatePost() public {
+        // Arrange
+        string memory content = "Hello, this is my first post!";
+
+        // Act
+        vm.prank(address(1));
+        socialMedia.createPost(content);
+
+        // Assert
+    SocialMedia.Post memory post = socialMedia.getPost(1);
+    uint id = post.id;
+    address author = post.author;
+    string memory retrievedContent = post.content;
+    uint timestamp = post.timestamp;
+
+        assertEq(id, 1);
+        assertEq(author, address(1));
+        assertEq(retrievedContent, content);
+        assertTrue(timestamp > 0);
+
+        // Check the event
+        vm.expectEmit(true, true, false, true);
+        emit SocialMedia.PostCreated(1, address(1), content, block.timestamp);
     }
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+    function testGetPost() public {
+    // Arrange
+    string memory content = "Hello, this is my first post!";
+
+    // Capture the block timestamp before creating the post
+    uint beforeTimestamp = block.timestamp;
+
+    // Act
+    vm.prank(address(1));
+    socialMedia.createPost(content);
+
+    // Assert
+    SocialMedia.Post memory post = socialMedia.getPost(1);
+    uint id = post.id;
+    address author = post.author;
+    string memory retrievedContent = post.content;
+    uint timestamp = post.timestamp;
+
+    assertEq(id, 1);
+    assertEq(author, address(1));
+    assertEq(retrievedContent, content);
+
+    // Assert that the timestamp is correct (it should be equal to or greater than the captured timestamp)
+    assertGe(timestamp, beforeTimestamp);
+
+    // Optional: Assert that the timestamp is not too far in the future (e.g., within 1 minute)
+    assertLe(timestamp, block.timestamp + 60);
+
+    // Check the event
+    vm.expectEmit(true, true, false, true);
+    emit SocialMedia.PostCreated(1, address(1), content, block.timestamp);
+    }
+
+    function testGetAllPosts() public {
+        // Arrange
+        string memory content1 = "First post";
+        string memory content2 = "Second post";
+
+        vm.prank(address(1));
+        socialMedia.createPost(content1);
+
+        vm.prank(address(2));
+        socialMedia.createPost(content2);
+
+        // Act
+        SocialMedia.Post[] memory allPosts = socialMedia.getAllPosts();
+
+        // Assert
+        assertEq(allPosts.length, 2);
+        assertEq(allPosts[0].content, content1);
+        assertEq(allPosts[1].content, content2);
     }
 }
